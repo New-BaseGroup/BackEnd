c0e4a6e7352f38c1b586634496c912fe195c9970
Adam Lord Eriksson <90420563+A-Lord@users.noreply.github.com>
2022-08-22 10:44:25 +0200

    added LoginDTO and login end point

--- FallProject/Controllers/AccountController.cs
+++ FallProject/Controllers/AccountController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
-
+using API.DTO;
 
 namespace API.Controllers
 {
@@ -9,7 +9,24 @@ namespace API.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        
-        
+        [AllowAnonymous] //will allow anyone to use this endpoint so that u dont have to be loged in to log in ;)
+        [HttpPost("login")]
+        public IActionResult Login(LoginDTO loginDto)
+        {
+            try 
+            {
+                //try loging service funktion
+
+                //if service returns true
+                return Ok("login success");
+                // else return Ok("login failed");
+                
+            }
+            catch (Exception ex)
+            {
+                //only activates on real errors
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
---
+++ FallProject/DTO/LoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace API.DTO
+{
+    public record LoginDTO
+    {
+
+        [Required(ErrorMessage = "{0} is a mandatory field")]
+        public string user { get; set; }
+
+
+        [Required(ErrorMessage = "{0} is a mandatory field")] 
+        public string password { get; init; }
+
+    }
+}
+ 
