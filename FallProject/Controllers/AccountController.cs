46751ca04cef39c157fd9a32316f319b75ce191a
Adam Lord Eriksson <90420563+A-Lord@users.noreply.github.com>
2022-08-22 12:25:55 +0200

    All user endpoints are upp
    With dummy data as service is not set up yet.

--- FallProject/Controllers/AccountController.cs
+++ FallProject/Controllers/AccountController.cs
@@ -11,28 +11,84 @@ namespace API.Controllers
     {
         [AllowAnonymous] //will allow anyone to use this endpoint so that u dont have to be loged in to log in ;)
         [HttpPost("login")]
-        public IActionResult Login(LoginDTO loginDto)
+        public IActionResult Login(LoginDTO loginDTO)
         {
-            try 
+            try
             {
                 //try loging service funktion
 
                 //if service returns true
-                if(loginDto.user == "admin" && loginDto.password == "adminadmin123!") // temporary check will be service function
-                return Ok(new
-                {
-                    status = "success",
-                    user = loginDto.user
-                });
+                if (loginDTO.User == "admin" && loginDTO.Password == "adminadmin123!") // temporary check will be service function
+                    return Ok(new
+                    {
+                        status = "success",
+                        message = loginDTO.User
+                    });
                 //returns a json objekt with status and user property
                 //always return same format on the object unless an actual error happens
                 else
                     return Ok(new
                     {
                         status = "failure",
-                        user = ""
+                        message = "wrong password or username"
                     });
                 // else return Ok("failure");
+
+            }
+            catch (Exception ex)
+            {
+                //only activates on real errors
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [AllowAnonymous] //will allow anyone to use this endpoint so that u dont have to be loged in to log in ;)
+        [HttpPost("register")]
+        public IActionResult Register(RegisterUserDTO registerUserDTO)
+        {
+            try
+            {
+                if (true) // temporary check if user is registered already  will  be a service function
+                    return Ok(new
+                    {
+                        status = "success",
+                        message = registerUserDTO.User
+                    });
+
+                else
+                    return Ok(new
+                    {
+                        status = "failure",
+                        message = "Username or email is already used"
+                    });
+
+            }
+            catch (Exception ex)
+            {
+                //only activates on real errors
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost("recover")]
+        public IActionResult Recover(NewPasswordDTO newPasswordDTO)
+        {
+            try
+            {
+                if(newPasswordDTO.Password != "hej") // temporary check if user is registered already  will  be a service function
+                    return Ok(new
+                    {
+                        status = "success",
+                        message = "New password saved"
+                    });
+
+                else
+                    return Ok(new
+                    {
+                        status = "failure",
+                        message = "Password is not secure"
+                    });
 
             }
             catch (Exception ex)
